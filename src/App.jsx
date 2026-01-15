686e5a6ef0ff600348d061c9e5b6c950d2d2d4aa
sourcecode69 <sourcecode1012@gmail.com>
2026-01-16 01:13:58 +0530

    Student Dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginPage from './LoginPage';
 import SignUpPage from './SignUpPage';
+import StudentDashboard from './pages/StudentDashboard';
+import { dummyUsers } from './data/dummyData';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+ import Navbar from './components/Navbar';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('login'); // 'login' or 'signup'
 
+  const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      // Check if user is logged in
+      const token = localStorage.getItem('token');
+      const userData = localStorage.getItem('user');
+      
+      if (token && userData) {
+        setUser(JSON.parse(userData));
+      } else {
+        console.log("Working");
+        // FOR DEMO: Auto-login as student
+        // Remove this in production!
+        const demoUser = dummyUsers.student;
+        setUser(demoUser);
+        console.log(demoUser)
+        // localStorage.setItem('user', JSON.stringify(demoUser));
+        // localStorage.setItem('token', 'demo_token_123');
+      }
+      setLoading(false);
+    }, []);
+  
+
   return (
-    <div>
-      {currentPage === 'login' ? (
+      <Router>
+     <div className="min-h-screen bg-gray-50">
+        <Navbar user={user} />
+        <div className="container mx-auto px-4 py-8">
+           <StudentDashboard user={user} />
+      {/* {currentPage === 'login' ? (
         <LoginPage onSignUpClick={() => setCurrentPage('signup')} />
       ) : (
         <SignUpPage onSignInClick={() => setCurrentPage('login')} />
-      )}
+      )} */}
     </div>
+    </div>
+    </Router>
   );
 }
 
